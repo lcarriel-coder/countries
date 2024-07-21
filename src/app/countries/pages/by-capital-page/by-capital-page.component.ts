99f65aff62641cd7b8d73e9447a597e433783499
Luis Carriel <imagetech.developer11@outlook.com>
2024-07-21 17:41:28 -0500

    avances

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-by-capital-page',
   templateUrl: './by-capital-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class ByCapitalPageComponent {
+  public countries: Country[] = [];
+  constructor(private countriesService: CountriesService) {}
 
-
-
-  searchBycapital( term:string){
-    console.log("Desde ByCapitalPage" , {term});
+  searchBycapital(term: string) {
+    this.countriesService.searchCapital(term).subscribe((countries) => {
+      this.countries = countries;
+    });
   }
-
 }
