cf6999c782d8b8531d7b4f485620f682105e64d1
Luis Carriel <imagetech.developer11@outlook.com>
2024-08-03 20:06:09 -0500

    .

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,21 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
+import { Region } from '../../interfaces/region.type';
+
 
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
-  styles: ``
+  styles: ``,
 })
-export class ByRegionPageComponent {
-
+export class ByRegionPageComponent implements OnInit{
   public countries: Country[] = [];
+  public regions: Region[] = [
+    'Africa',
+    'Americas',
+    'Asia',
+    'Europe',
+    'Oceania',
+  ];
+  public selectedRegion?: Region;
+
   constructor(private countriesService: CountriesService) {}
 
-  searchByregion(term: string) {
-    this.countriesService.searchRegion(term).subscribe((countries) => {
+  ngOnInit(): void {
+     this.countries = this.countriesService.cacheStore.byRegion.countries;
+     this.selectedRegion = this.countriesService.cacheStore.byRegion.region;
+  }
+
+  searchByregion(region: Region) {
+    this.selectedRegion = region;
+    this.countriesService.searchRegion(region).subscribe((countries) => {
       this.countries = countries;
     });
   }
-
 }
